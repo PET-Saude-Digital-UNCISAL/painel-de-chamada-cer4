28f0c8e88a733afe97e248f204ec79a32eb2d496
DanielyVasconcelos <daniely.vasconcelos@yellowkite.digital>
2026-07-11 13:04:05 -0300

    fix: remove dependencia do manifesto de arquivos estaticos

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,6 @@ STORAGES = {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
