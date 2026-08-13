0bf6df9a987d5e72b05671ce1492fe7ed99a5a74
Daniely Vasconcelos <daniely.vasconcelos@academico.uncisal.edu.br>
2026-08-13 10:24:45 -0300

    temp: rodar seed_usuarios no deploy (reverter depois)

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ set -o errexit
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+python manage.py seed_usuarios
 exec daphne config.asgi:application --bind "0.0.0.0" --port "${PORT:-8000}"
